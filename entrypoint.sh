694bb65e8164f804d0c4cc7f648a922285e91666
adamkoro <adam.koronics@gmail.com>
2023-12-13 22:11:14 +0100

    feat: refactor and use s3cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,104 +1,91 @@
 #!/bin/sh
 CURRENT_DATE=$(date +%Y-%m-%d_%H-%M-%S)
-S3_ALIAS_NAME="docker-backup"
 
-echo "Starting backup"
-echo "Current date: ${CURRENT_DATE}"
-echo "Environment variables checks started"
+infoMessage()
+{
+    echo "INFO: ${1}"
+}
+
+erroMessage()
+{
+    echo "ERROR: ${1}"
+    exit 1
+}
+
+checkVariable(){
+    if [ -z "${1}" ]; then
+        erroMessage "${1} is not set"
+    fi
+}
+
+infoMessage "Starting backup"
+infoMessage "Current date: ${CURRENT_DATE}"
+infoMessage "Environment variables checks started"
+
 # Check envs are not empty
-if [ -z "$BACKUP_DIR" ]; then
-    echo "BACKUP_DIR is not set"
-    exit 1
-fi
-
-if [ -z "$S3_BUCKET" ]; then
-    echo "S3_BUCKET is not set"
-    exit 1
-fi
-
-if [ -z "$S3_ENDPOINT" ]; then
-    echo "S3_ENDPOINT is not set"
-    exit 1
-fi
-
-if [ -z "$S3_ACCESS_KEY" ]; then
-    echo "S3_ACCESS_KEY is not set"
-    exit 1
-fi
-
-if [ -z "$S3_SECRET_KEY" ]; then
-    echo "S3_SECRET_KEY is not set"
-    exit 1
-fi
-
-if [ -z "$S3_REGION" ]; then
-    echo "S3_REGION is not set"
-    exit 1
-fi
-
-if [ -z "$S3_SSL" ]; then
-    echo "S3_SSL is not set"
-    exit 1
-fi
-
-if [ -z "$BACKUP_NAME" ]; then
-    echo "BACKUP_NAME is not set"
-    exit 1
-fi
+checkVariable "$S3_BUCKET"
+checkVariable "$S3_ENDPOINT"
+checkVariable "$S3_ACCESS_KEY"
+checkVariable "$S3_ACCESS_KEY"
+checkVariable "$S3_REGION"
+checkVariable "$S3_SSL"
+checkVariable "$BACKUP_NAME"
+checkVariable "$BACKUP_DIR"
 
 # Check if backup dir exists
 if [ ! -d "$BACKUP_DIR" ]; then
-    echo "Backup directory does not exist"
-    exit 1
+    erroMessage "Backup directory does not exist"
 fi
-echo "Environment variables checks completed"
+infoMessage "Environment variables checks completed"
 
 # Check if backup dir is empty
 if [ ! "$(ls -A "${BACKUP_DIR}")" ]; then
-    echo "Backup directory is empty"
-    exit 1
+    erroMessage "Backup directory is empty"
 fi
 
-# Check if minio-client is installed
-if ! command -v mc > /dev/null 2>&1
-then
-    echo "Minio-client could not be found"
-    exit 1
+# Check if s3cmd is installed
+if ! command -v s3cmd > /dev/null 2>&1; then
+    erroMessage "s3cmd could not be found"
 fi
 
-# Create alias for s3 and check for ssl
-echo "Creating alias for S3"
-if [ "${S3_SSL}" = "true" ]; then
-    S3_ENDPOINT="https://${S3_ENDPOINT}"
+# Create the .s3cfg file
+cat > ~/.s3cfg << EOF
+[default]
+access_key = ${S3_ACCESS_KEY}
+secret_key = ${S3_SECRET_KEY}
+bucket_location = ${S3_REGION}
+use_https = ${S3_SSL}
+host_base = ${S3_ENDPOINT}
+host_bucket = ${S3_ENDPOINT}
+EOF
+
+# Check if bucket exists
+if [ "${S3_SSL}" = true ]; then
+    DEFAULT_URL_PREFIX=https
 else
-    S3_ENDPOINT="http://${S3_ENDPOINT}"
+    DEFAULT_URL_PREFIX=http
 fi
-if ! mc alias set "${S3_ALIAS_NAME}" "${S3_ENDPOINT}" "${S3_ACCESS_KEY}" "${S3_SECRET_KEY}"; then
-    echo "Could not create alias for S3"
-    exit 1
-fi
-echo "Alias created successfully"
 
-# Ping s3
-echo "Pinging S3"
-if ! mc ping -q --count 3 "${S3_ALIAS_NAME}"; then
-    echo "Could not connect to S3"
-    exit 1
+# Create the bucket if it does not exist
+if ! s3cmd la --host="${DEFAULT_URL_PREFIX}://${S3_ENDPOINT#*.}" | grep "${S3_BUCKET}" > /dev/null; then
+    if ! s3cmd mb s3://"${S3_BUCKET}" > /dev/null; then
+        erroMessage "Could not create bucket"
+    fi
+    infoMessage "Bucket created successfully"
+else
+    infoMessage "Bucket already exists"
 fi
-echo "S3 pinged successfully"
 
 # Create tar.gz file
-echo "Creating tar.gz file"
+infoMessage "Creating tar.gz file"
 if ! tar -czf "/tmp/${BACKUP_NAME}_${CURRENT_DATE}.tar.gz" --exclude="${BACKUP_NAME}_${CURRENT_DATE}.tar.gz" ./; then
-    echo "Could not create tar.gz file"
-    exit 1
+    erroMessage "Could not create tar.gz file"
 fi
-echo "Tar.gz file created successfully"
+infoMessage "Tar.gz file created successfully"
 
 # Upload backup file to s3
-echo "Uploading backup file to S3"
-if ! mc cp "/tmp/${BACKUP_NAME}_${CURRENT_DATE}.tar.gz" "${S3_ALIAS_NAME}/${S3_BUCKET}"; then
-    echo "Could not upload backup file to S3"
-    exit 1
+infoMessage "Uploading backup file to S3"
+if ! s3cmd put "/tmp/${BACKUP_NAME}_${CURRENT_DATE}.tar.gz" "s3://${S3_BUCKET}" > /dev/null; then
+    erroMessage "Could not upload backup file to S3"
 fi
-echo "Backup completed successfully"
+infoMessage "Backup completed successfully"
